01c71046aa386ffbc43d3120c2cf119d4b2f9b24
veganedge <scottxbrown@yahoo.com>
2021-07-03 11:38:56 -0700

    changed fetch locations to heroku

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://fast-woodland-90967.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://fast-woodland-90967.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
